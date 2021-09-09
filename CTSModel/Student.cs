9e9b25d4143b33d4ec8f13d19cf0171c62659445
Divya Sivan <divyas.d214@gmail.com>
2021-09-09 18:18:54 +0530

    first commit

---
+++ CTSModel/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace apiForAzure.CTSModel
+{
+    public partial class Student
+    {
+        public int StudId { get; set; }
+        public string StudName { get; set; }
+        public string StudAddr { get; set; }
+        public double? Marks { get; set; }
+    }
+}
---
+++ CTSModel/collegeContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace apiForAzure.CTSModel
+{
+    public partial class collegeContext : DbContext
+    {
+        public collegeContext()
+        {
+        }
+
+        public collegeContext(DbContextOptions<collegeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=.;Database=college;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AI");
+
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.HasKey(e => e.StudId)
+                    .HasName("PK__Student__09AFA909C00E882D");
+
+                entity.ToTable("Student");
+
+                entity.Property(e => e.StudId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("Stud_id");
+
+                entity.Property(e => e.Marks).HasColumnName("marks");
+
+                entity.Property(e => e.StudAddr)
+                    .HasMaxLength(30)
+                    .IsUnicode(false)
+                    .HasColumnName("Stud_addr");
+
+                entity.Property(e => e.StudName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("Stud_name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
